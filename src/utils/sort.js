df7310758b456ec074a3f3a27eb41f29da605d90
Igor Belogurov <belogurovigor@gmail.com>
2019-09-01 20:40:48 +0300

    sort.js: рефакторинг

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,11 +1,12 @@
 const sortUp = (key) => (a, b) => a[key] - b[key];
 const sortDown = (key) => (a, b) => b[key] - a[key];
 
-export const getSortedTasks = (tasks, sortType) => {
-  const sort = ({
-    'date-up': () => tasks.sort(sortUp(`dueDate`)),
-    'date-down': () => tasks.sort(sortDown(`dueDate`)),
-  });
+const sort = (tasks) => ({
+  "date-up": () => tasks.sort(sortUp(`dueDate`)),
+  "date-down": () => tasks.sort(sortDown(`dueDate`)),
+});
 
-  return sort[sortType] ? sort[sortType]() : tasks;
+export const getSortedTasks = (tasks, sortType) => {
+  const sortTasks = sort(tasks);
+  return sortTasks[sortType] ? sortTasks[sortType]() : tasks;
 };
