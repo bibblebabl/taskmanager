ced26cb5dafddf790b53295ffbb459fb2dcb0831
Igor <belogurovigor@gmail.com>
2019-09-05 17:54:58 +0300

    webpack.config.js: убрал лишние локали из плагина MomentLocalesPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,6 @@ module.exports = {
     watchContentBase: true
   },
   plugins: [
-    new MomentLocalesPlugin({
-      localesToKeep: [`es-us`, `ru`],
-    }),
-  ],
+    new MomentLocalesPlugin()
+  ]
 };
